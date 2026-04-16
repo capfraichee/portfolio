436a67e00b18b770432d85d98abc9c818e0e4b76
capfraichee <casras01@easv365.dk>
2026-04-16 10:05:56 +0200

    (fixed) background color

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="grid grid-cols-12 content-start gap-1 px-25 min-h-screen">
-    <header class="ml-2 col-span-12 h-15 align-center flex items-center mt-20">
+  <div class="grid grid-cols-12 content-start gap-1 px-25 min-h-screen bg-[var(--background-color-dark)]">
+    <header class="ml-2 col-span-12 h-15 align-center flex items-center">
       <nav class="flex flex-row gap-6" style="font-family: 'Mulish', sans-serif; font-weight: 600; text-transform: uppercase; font-size: 20px;">
-        <RouterLink to="/" class="p-2 rounded">Hjernen bag</RouterLink>
-        <!-- <RouterLink to="/about" class="p-2 rounded">Mig</RouterLink> -->
+        <RouterLink to="/" class="p-2 rounded">Forside</RouterLink>
+        <RouterLink to="/about" class="p-2 rounded">Om mig</RouterLink>
         <RouterLink to="/school-projects" class="p-2 rounded ">Skole Projekter</RouterLink>
         <RouterLink to="/personal-projects" class="p-2 rounded ">Personlige Projekter</RouterLink>
       </nav>
